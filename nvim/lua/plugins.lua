b3a4960cccf9130ea8e2e0d47268c00b1b0a05d9
Syed M. Abbas Haider Taqvi <abbastaqvi81@gmail.com>
2024-05-31 04:16:55 -0700

    Delete nvim directory

--- nvim/lua/plugins.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "lervag/vimtex",
-    "wakatime/vim-wakatime",
-    "tpope/vim-surround",
-    "github/copilot.vim",
-    "pocco81/auto-save.nvim",
-}
--- nvim/lua/plugins/transparent.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "xiyaowong/nvim-transparent",
-    config = function()
-        vim.keymap.set("n", "<leader>tt", vim.cmd.TransparentToggle, { silent = true }) -- toggle transparent
-    end
-}
